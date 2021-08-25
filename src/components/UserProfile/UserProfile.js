babe26884bd4cb979353f20f8e44a7ca68d936c4
MdNakibulHosenNahid <56796558+MdNakibulHosenNahid@users.noreply.github.com>
2021-08-25 17:06:03 +0600

    user profile component added

---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,31 @@
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../../App';
+
+const UserProfile = () => {
+    // Get UserContext 
+    const [userInfo,] = useContext(UserContext)
+    // Get user's profile 
+    const [userProfile,setUserProfile] = useState({})
+    fetch('https://strofesapps.live/challenge/user/profile',{
+        method: 'GET',
+        headers:{
+            'Content-Type': 'application/json',
+            'token' : userInfo.token,
+        },
+    }).then(res => res.json())
+    .then(data =>{
+        setUserProfile(data);
+        console.log(userProfile);
+    });
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h2 className="text-center pt-5">Welcome Back!</h2>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default UserProfile;
